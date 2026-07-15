d8dc578e7ba7d93577c3d5b54e8dbeda8ae999c9
lilkourosh <koroushkhaleghii@gmail.com>
2026-07-14 17:47:45 -0700

    Creating a dictionary folder and starting the exerxises on the topic of Dictionaries.

---
+++ Dictionary/student_info.py
@@ -0,0 +1,11 @@
+# Exercise 1: Create a dictionary for student information
+
+student = {
+    "name": "Kourosh Khaleghi",
+    "age": 22,
+    "student_id": "9823105",
+    "courses": ["Python", "Data Structures"]
+}
+
+print("Student Information:")
+print(student)
